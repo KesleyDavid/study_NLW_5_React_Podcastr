40f1060b5573df2f4c2a724e1f92b8309c44f423
Kesley David <kesleygti@gmail.com>
2021-04-22 09:21:20 -0300

    [web] js to ts

---
+++ web/pages/api/hello.ts
@@ -0,0 +1,9 @@
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Route => http://localhost:3000/api/hello
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({ name: 'Kesley David' })
+}
